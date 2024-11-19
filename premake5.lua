cf410fa7de904d223f162fcb4b8a52a9c5a80ed2
Hoang Huu <89367868+huuhoangcong@users.noreply.github.com>
2024-11-20 02:30:41 +0900

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,6 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
-    staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -24,6 +23,7 @@ project "ImGui"
 	filter "system:windows"
 		systemversion "latest"
 		cppdialect "C++17"
+		staticruntime "On"
 
 	filter "system:linux"
 		pic "On"
